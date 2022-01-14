2412c6c7ed03d65caa2c13bd3a873b6a5ff427bc
romchik224 <eduard.babisko@gmail.com>
2022-01-14 09:18:25 +0100

    Nachricht geändert

--- Four in the row/src/Version1.java
+++ Four in the row/src/Version1.java
@@ -2,8 +2,8 @@
 public class Version1 {
 
 	public static void main(String[] args) {
-		System.out.println("In Bremen sagt man Moin");
-
+		System.out.println("In Bremen sagt man immer Moin");
+		
 	}
 
 }
